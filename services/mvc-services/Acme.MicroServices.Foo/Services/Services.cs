83f14782d029b87c1869f91f26dffb0b1bcba367
Paul Anderson <paul.anderson@legendware.co.uk>
2019-11-05 16:15:40 +0000

    initial web api based solution

---
+++ services/mvc-services/Acme.MicroServices.Foo/Services/Services.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acme.MicroServices.Foo.Services
+{
+    public class AlphaService : IAlphaService
+    {
+    }
+
+    public interface IAlphaService
+    {
+    }
+}
