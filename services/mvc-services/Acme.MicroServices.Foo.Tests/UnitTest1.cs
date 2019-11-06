3176b38d44ccf828ff24c15b25de5d9b0cf81c9d
andez2000 <andez2000@hotmail.com>
2019-11-06 20:12:42 +0000

    test nunit

--- services/mvc-services/Acme.MicroServices.Foo.Tests/UnitTest1.cs
+++ services/mvc-services/Acme.MicroServices.Foo.Tests/UnitTest1.cs
@@ -1,14 +1,41 @@
+using Acme.MicroServices.Foo.Entities;
+using Acme.MicroServices.Foo.Repositories;
+using Acme.MicroServices.Foo.Services;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
 using System;
-using Xunit;
 
 namespace Acme.MicroServices.Foo.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+	[TestFixture]
+    public class AlphaServiceTests
+	{
+		private AlphaService _service;
+		private Mock<IAlphaRepository> _alphaRepository;
 
-        }
+		[SetUp]
+		public void Setup()
+		{
+			_alphaRepository = new Mock<IAlphaRepository>();
+			_service = new AlphaService(_alphaRepository.Object);
+		}
+
+		[Test]
+        public void SumReturnsCorrectTotal()
+        {
+			var alphas = new[]
+			{
+				new Alpha { Id = Guid.Empty, Name = "1",  Number = 3 },
+				new Alpha { Id = Guid.Empty, Name = "2",  Number = 2 },
+			};
+
+			_alphaRepository.Setup(m => m.GetAll()).Returns(alphas);
+
+			var result = _service.GetTotal();
+
+			result.Should().Be(5);
+
+		}
     }
 }
